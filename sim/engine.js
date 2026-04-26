02710162943b729747867f43383a3eaef70abd22
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-04-26 20:14:45 +0800

    Update engine.js

--- sim/engine.js
+++ sim/engine.js
@@ -4,7 +4,6 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-
 class Vector {
 
     constructor(x = 0, y = 0) 
@@ -17,6 +16,15 @@ class Vector {
     {
         this.x += v.x;
         this.y += v.y;
+
+        return this;
+    }
+
+    subtract(v) 
+    {
+        this.x -= v.x;
+        this.y -= v.y;
+
         return this;
     }
 
@@ -24,7 +32,31 @@ class Vector {
     {
         this.x *= s;
         this.y *= s;
+
         return this;
+    }
+
+    length() 
+    {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    normalize() 
+    {
+        const len = this.length();
+
+        if (len !== 0) 
+        {
+            this.x /= len;
+            this.y /= len;
+        }
+
+        return this;
+    }
+
+    dot(v) 
+    {
+        return this.x * v.x + this.y * v.y;
     }
 
     clone() 
@@ -34,29 +66,34 @@ class Vector {
 }
 
 
-class Ball {
+class Ball 
+{
     constructor(x, y) 
     {
+
         this.position = new Vector(x, y);
-        
-        this.velocity = new Vector((Math.random() - 0.5) * 200, Math.random() * -200);
-        
+
+        this.velocity = new Vector((Math.random() - 0.5) * 200, Math.random() * -200 );
+
         this.radius = 10 + Math.random() * 10;
-        
+
         this.gravity = new Vector(0, 500);
-        
+
         this.friction = 0.7;
+
+        //Random color
+        this.color = `hsl(${Math.random() * 360}, 70%, 60%)`;
     }
 
     update(dt) 
     {
-        //gravity
         this.velocity.add(this.gravity.clone().multiply(dt));
+
         this.position.add(this.velocity.clone().multiply(dt));
 
-        //collision
         if (this.position.y + this.radius > canvas.height) 
         {
+
             this.position.y = canvas.height - this.radius;
             this.velocity.y *= -this.friction;
         }
@@ -67,37 +104,83 @@ class Ball {
         }
     }
 
-    draw() 
-    {
+    draw() {
         ctx.beginPath();
         ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
-        ctx.fillStyle = "white";
+        ctx.fillStyle = this.color;
         ctx.fill();
     }
 }
+
+
+function resolveCollision(b1, b2) 
+{
+
+    const normal = b2.position.clone().subtract(b1.position);
+
+    const dist = normal.length();
+
+    const minDist = b1.radius + b2.radius;
+
+    if (dist < minDist) 
+    {
+
+        const n = normal.clone().normalize();
+
+        //separate overlapping balls
+        const overlap = minDist - dist;
+        b1.position.add(n.clone().multiply(-overlap / 2));
+        b2.position.add(n.clone().multiply(overlap / 2));
+
+        //velocity
+        const relativeVelocity = b2.velocity.clone().subtract(b1.velocity);
+
+        const speed = relativeVelocity.dot(n);
+
+        if (speed > 0)
+        {
+            return;
+        }
+
+        const impulse = -2 * speed / 2;
+
+        const impulseVector = n.clone().multiply(impulse);
+
+        b1.velocity.subtract(impulseVector);
+        b2.velocity.add(impulseVector);
+    }
+}
+
 
 const balls = [];
 
 let lastTime = 0;
 
+//mouse
 let mouseDown = false;
 let startPos = null;
+let currentMouse = null;
 
 canvas.addEventListener("mousedown", (e) => {
     mouseDown = true;
     startPos = new Vector(e.clientX, e.clientY);
 });
 
+canvas.addEventListener("mousemove", (e) => {
+    currentMouse = new Vector(e.clientX, e.clientY);
+});
+
 canvas.addEventListener("mouseup", (e) => {
 
     if (!mouseDown) 
     {
-        return; 
+        return;
     }
 
     const endPos = new Vector(e.clientX, e.clientY);
 
-    const velocity = new Vector((endPos.x - startPos.x) * 5,(endPos.y - startPos.y) * 5);
+    const velocity = new Vector((endPos.x - startPos.x) * 5,
+        (endPos.y - startPos.y) * 5 );
 
     const ball = new Ball(startPos.x, startPos.y);
     ball.velocity = velocity;
@@ -107,19 +190,54 @@ canvas.addEventListener("mouseup", (e) => {
     mouseDown = false;
 });
 
+//animation
+
 function animate(time) 
 {
 
-    const dt = (time - lastTime) / 1000; 
+    const dt = (time - lastTime) / 1000;
     lastTime = time;
 
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    //update
     for (let ball of balls) 
     {
         ball.update(dt);
+    }
+
+    //collisions
+    for (let i = 0; i < balls.length; i++) 
+    {
+
+        for (let j = i + 1; j < balls.length; j++) 
+        {
+
+            resolveCollision(balls[i], balls[j]);
+        }
+    }
+
+    for (let ball of balls) 
+    {
         ball.draw();
+    }
+
+    // Draw preview line
+    if (mouseDown && startPos && currentMouse) 
+    {
+
+        ctx.beginPath();
+
+        ctx.moveTo(startPos.x, startPos.y);
+
+        ctx.lineTo(currentMouse.x, currentMouse.y);
+
+        ctx.strokeStyle = "white";
+
+        ctx.lineWidth = 2;
+        
+        ctx.stroke();
     }
 
     requestAnimationFrame(animate);
