d03a05ae4959d2156b6e9b92ee26c8eb9ced6cb3
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-06-12 03:19:10 +0800

    Create clocks.js

---
+++ clocks.js
@@ -0,0 +1,67 @@
+import { warn } from '../utils.js';
+
+class Clock {
+
+	constructor( autoStart = true ) {
+
+		this.autoStart = autoStart;
+		this.startTime = 0;
+		this.oldTime = 0;
+		this.elapsedTime = 0;
+		this.running = false;
+
+		warn( 'Clock: This module has been deprecated. Please use THREE.Timer instead.' ); // @deprecated, r183
+
+	}
+	start() {
+
+		this.startTime = performance.now();
+		this.oldTime = this.startTime;
+		this.elapsedTime = 0;
+		this.running = true;
+
+	}
+
+	stop() {
+
+		this.getElapsedTime();
+		this.running = false;
+		this.autoStart = false;
+
+	}
+  
+	getElapsedTime() {
+
+		this.getDelta();
+		return this.elapsedTime;
+
+	}
+
+	getDelta() {
+
+		let diff = 0;
+
+		if ( this.autoStart && ! this.running ) {
+
+			this.start();
+			return 0;
+
+		}
+
+		if ( this.running ) {
+
+			const newTime = performance.now();
+
+			diff = ( newTime - this.oldTime ) / 1000;
+			this.oldTime = newTime;
+			this.elapsedTime += diff;
+
+		}
+
+		return diff;
+
+	}
+
+}
+
+export { Clock };
